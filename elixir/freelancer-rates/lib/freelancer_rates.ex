a393739917bf861cb43684416bbe487e79588937
f u n y a r i n p a <havenoreasontolive@gmail.com>
2023-02-07 15:21:48 +0300

    add: `monthly_rate`

--- elixir/freelancer-rates/lib/freelancer_rates.ex
+++ elixir/freelancer-rates/lib/freelancer_rates.ex
@@ -9,7 +9,8 @@ defmodule FreelancerRates do
   end
 
   def monthly_rate(hourly_rate, discount) do
-    daily_rate(hourly_rate) * 22 - discount
+    before_discount = daily_rate(hourly_rate) * 22
+    trunc(ceil(apply_discount(before_discount, discount)))
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
