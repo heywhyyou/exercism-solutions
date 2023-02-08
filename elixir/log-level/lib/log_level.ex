a6378bd927b7e1a0ca6368ba35314c9bceeb43bb
f u n y a r i n p a <havenoreasontolive@gmail.com>
2023-02-08 14:15:01 +0300

    add: cond for level 0 in `to_label/2`

--- elixir/log-level/lib/log_level.ex
+++ elixir/log-level/lib/log_level.ex
@@ -1,6 +1,9 @@
 defmodule LogLevel do
   def to_label(level, legacy?) do
-    # Please implement the to_label/2 function
+    cond do
+      level == 0 and not legacy? -> :trace
+      true -> :unknown
+    end
   end
 
   def alert_recipient(level, legacy?) do
