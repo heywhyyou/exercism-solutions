42b48e6e300734752ca8ff8948cd35703722ff79
f u n y a r i n p a <havenoreasontolive@gmail.com>
2023-02-10 14:32:50 +0300

    add `simpleTicketStatus`

--- javascript/amusement-park/amusement-park.js
+++ javascript/amusement-park/amusement-park.js
@@ -55,7 +55,11 @@ export function ticketStatus(tickets, ticketId) {
  * @returns {string} ticket status
  */
 export function simpleTicketStatus(tickets, ticketId) {
-  throw new Error("Please implement the simpleTicketStatus function.");
+  if (tickets[ticketId] == null || tickets[ticketId] == undefined) {
+    return "invalid ticket !!!";
+  } else {
+    return tickets[ticketId];
+  }
 }
 
 /**
