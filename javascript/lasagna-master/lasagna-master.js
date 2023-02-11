8848b5408b450631956b5a769038f88cc7e4740e
f u n y a r i n p a <havenoreasontolive@gmail.com>
2023-02-11 16:09:11 +0300

    add: `cookingStatus()`

--- javascript/lasagna-master/lasagna-master.js
+++ javascript/lasagna-master/lasagna-master.js
@@ -10,3 +10,13 @@
  *   ...
  * }
  */
+
+export function cookingStatus(timer) {
+  if (timer > 0) {
+    return "Not done, please wait.";
+  } else if (timer === 0) {
+    return "Lasagna is done.";
+  } else {
+    return "You forgot to set the timer.";
+  }
+}
