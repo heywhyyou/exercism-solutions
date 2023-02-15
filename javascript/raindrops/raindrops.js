41b3b1f24de4eb9bcd4cc59aa60bf73c58b740cb
f u n y a r i n p a <havenoreasontolive@gmail.com>
2023-02-15 17:28:21 +0300

    feat: add convert function

--- javascript/raindrops/raindrops.js
+++ javascript/raindrops/raindrops.js
@@ -3,6 +3,21 @@
 // convenience to get you started writing code faster.
 //
 
-export const convert = () => {
-  throw new Error('Remove this statement and implement this function');
+export const convert = (num) => {
+  let rainDrops = "";
+  if (num % 3 === 0) {
+    rainDrops += "Pling";
+  }
+  if (num % 5 === 0) {
+    rainDrops += "Plang";
+  }
+  if (num % 7 === 0) {
+    rainDrops += "Plong";
+  }
+
+  if (num % 3 === 0 || num % 5 === 0 || num % 7 === 0) {
+    return rainDrops;
+  } else {
+    return num.toString();
+  }
 };
