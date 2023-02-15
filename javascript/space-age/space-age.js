98312ba4dc1abb69db499402790e0e3b8738d163
f u n y a r i n p a <havenoreasontolive@gmail.com>
2023-02-15 14:07:03 +0300

    feat: implement age function

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -3,6 +3,23 @@
 // convenience to get you started writing code faster.
 //
 
-export const age = () => {
-  throw new Error('Remove this statement and implement this function');
+export const age = (planet, seconds) => {
+  const orbitalPeriodEarth = 31557600;
+  if (planet === "earth") {
+    return Number((seconds / orbitalPeriodEarth).toFixed(2));
+  } else if (planet === "mercury") {
+    return Number((seconds / (orbitalPeriodEarth * 0.2408467)).toFixed(2));
+  } else if (planet === "venus") {
+    return Number((seconds / (orbitalPeriodEarth * 0.61519726)).toFixed(2));
+  } else if (planet === "mars") {
+    return Number((seconds / (orbitalPeriodEarth * 1.8808158)).toFixed(2));
+  } else if (planet === "jupiter") {
+    return Number((seconds / (orbitalPeriodEarth * 11.862615)).toFixed(2));
+  } else if (planet === "saturn") {
+    return Number((seconds / (orbitalPeriodEarth * 29.447498)).toFixed(2));
+  } else if (planet === "uranus") {
+    return Number((seconds / (orbitalPeriodEarth * 84.016846)).toFixed(2));
+  } else if (planet === "neptune") {
+    return Number((seconds / (orbitalPeriodEarth * 164.79132)).toFixed(2));
+  }
 };
