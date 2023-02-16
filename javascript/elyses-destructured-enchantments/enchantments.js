942487b1e043926c071887173a13b8f2699532af
f u n y a r i n p a <havenoreasontolive@gmail.com>
2023-02-16 14:19:21 +0300

    feat: add getFirstCard(deck) function
    also some regular Prettier formatting stuff

--- javascript/elyses-destructured-enchantments/enchantments.js
+++ javascript/elyses-destructured-enchantments/enchantments.js
@@ -9,7 +9,8 @@
  * @returns {Card} the first card in the deck
  */
 export function getFirstCard(deck) {
-  throw new Error('Implement the getFirstCard function');
+  const [firstCard, ...everythingElse] = deck;
+  return firstCard;
 }
 
 /**
@@ -20,7 +21,7 @@ export function getFirstCard(deck) {
  * @returns {Card} the second card in the deck
  */
 export function getSecondCard(deck) {
-  throw new Error('Implement the getSecondCard function');
+  throw new Error("Implement the getSecondCard function");
 }
 
 /**
@@ -31,7 +32,7 @@ export function getSecondCard(deck) {
  * @returns {Card[]} new deck with reordered cards
  */
 export function swapTopTwoCards(deck) {
-  throw new Error('Implement the swapTopTwoCards function');
+  throw new Error("Implement the swapTopTwoCards function");
 }
 
 /**
@@ -43,11 +44,11 @@ export function swapTopTwoCards(deck) {
  * deck and a new deck containing all the other cards
  */
 export function discardTopCard(deck) {
-  throw new Error('Implement the discardTopCard function');
+  throw new Error("Implement the discardTopCard function");
 }
 
 /** @type Card[] **/
-const FACE_CARDS = ['jack', 'queen', 'king'];
+const FACE_CARDS = ["jack", "queen", "king"];
 
 /**
  * Insert face cards into the given deck
@@ -58,5 +59,5 @@ const FACE_CARDS = ['jack', 'queen', 'king'];
  * third, and fourth cards are the face cards
  */
 export function insertFaceCards(deck) {
-  throw new Error('Implement the insertFaceCards function');
+  throw new Error("Implement the insertFaceCards function");
 }
