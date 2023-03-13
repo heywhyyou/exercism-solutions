e5590986082cc2fc80146fdac8f328a8d6be1398
heywhyyou <havenoreasontolive@gmail.com>
2023-03-13 04:50:12 +0300

    feat: add comments

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,9 +1,13 @@
-package weather
+// Package weather is a collection of forecast related source files.
+package weather 
 
-var CurrentCondition string
-var CurrentLocation string
+// CurrentCondition stores the value of current condition of defined location.
+var CurrentCondition string 
+// CurrentLocation stores the name of the current location.
+var CurrentLocation string 
 
-func Forecast(city, condition string) string {
+// Forecast function makes the forecast happen.
+func Forecast(city, condition string) string {  
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
-}
+} 
