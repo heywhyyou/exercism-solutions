f35310409f663187770dabf77ac07978804a5c18
heywhyyou <havenoreasontolive@gmail.com>
2023-03-14 22:38:05 +0300

    feat: implement reverseString()

--- javascript/reverse-string/reverse-string.js
+++ javascript/reverse-string/reverse-string.js
@@ -3,6 +3,10 @@
 // convenience to get you started writing code faster.
 //
 
-export const reverseString = () => {
-  throw new Error('Remove this statement and implement this function');
+export const reverseString = (string) => {
+  let reversed = "";
+  for (let i = 0; i < string.length; i++) {
+    reversed = `${string[i]}${reversed}`;
+  }
+  return reversed;
 };
